64e2d8c2cbc54d9e65aff38baf0b1985b5e23b62
Jonathan Mori <jonathanmori@hotmail.com>
2020-12-03 22:55:03 -0300

    .

--- src/test/java/com/comprascrud/ComprasCRUD/ComprasCrudApplicationTests.java
+++ src/test/java/com/comprascrud/ComprasCRUD/ComprasCrudApplicationTests.java
@@ -3,7 +3,9 @@ package com.comprascrud.ComprasCRUD;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.avaliacaocomprascrud.MainApplication;
+
+@SpringBootTest(classes = MainApplication.class)
 class ComprasCrudApplicationTests {
 
 	@Test
